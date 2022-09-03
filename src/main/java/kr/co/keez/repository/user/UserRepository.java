5c8024e96bae4872fdf96f560adb8576d860a17d
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-09-03 15:41:11 +0900

    feat: 유저 도메인 초기 설정

---
+++ src/main/java/kr/co/keez/repository/user/UserRepository.java
@@ -0,0 +1,10 @@
+package kr.co.keez.repository.user;
+
+import kr.co.keez.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
