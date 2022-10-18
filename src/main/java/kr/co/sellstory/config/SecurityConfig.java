2e7c8fa52065aa644d6332c6aece459287018191
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-18 19:37:07 +0900

    fix: import 삭제 및 코드 정리

--- src/main/java/kr/co/sellstory/config/SecurityConfig.java
+++ src/main/java/kr/co/sellstory/config/SecurityConfig.java
@@ -1,25 +1,22 @@
 package kr.co.sellstory.config;
 
-import kr.co.sellstory.handler.OAuth2SuccessHandler;
-import kr.co.sellstory.service.auth.CustomOAuth2UserService;
-import kr.co.sellstory.service.jwt.JwtAuthFilter;
+import kr.co.sellstory.service.jwt.JwtAuthenticationFilter;
 import kr.co.sellstory.service.jwt.JwtTokenService;
-import kr.co.sellstory.service.jwt.TempJwtTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Slf4j
@@ -42,49 +39,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    private final CustomOAuth2UserService oAuth2UserService;
-    private final OAuth2SuccessHandler successHandler;
     private final JwtTokenService jwtTokenService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http
+                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
+                .csrf().disable() // csrf 보안 토큰 disable처리.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
                 .and()
-                .authorizeRequests()
-                .antMatchers("/token/**").permitAll()
-                .anyRequest().authenticated()
+                .authorizeRequests().requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // 요청에 대한 사용권한 체크
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                // TODO: 유저 권한 설정 방법으로 추후 필요해지면 수정
+                //.antMatchers("/user/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
                 .and()
-                .addFilterBefore(new JwtAuthFilter(jwtTokenService),
-                        UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login().loginPage("/token/expired")
-                .successHandler(successHandler)
-                .userInfoEndpoint().userService(oAuth2UserService);
-
-        http.addFilterBefore(new JwtAuthFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class);
+                .cors()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService),
+                        UsernamePasswordAuthenticationFilter.class);
+        // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
     }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
-//                .csrf().disable() // csrf 보안 토큰 disable처리.
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
-//                .and()
-//                .authorizeRequests().requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // 요청에 대한 사용권한 체크
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-//                // TODO: 유저 권한 설정 방법으로 추후 필요해지면 수정
-//                //.antMatchers("/user/**").hasRole("USER")
-//                .antMatchers(HttpMethod.GET).permitAll()
-//                .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
-//                .and()
-//                .cors()
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-//                        UsernamePasswordAuthenticationFilter.class);
-//        // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
-//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
