92efab8494cf6b20ffd78330ac7c7f83687d71dc
ParkJongHa <joshua_0621@naver.com>
2022-10-23 14:49:36 +0900

    added BcryptPasswordEncoder

--- src/main/java/kr/co/sellstory/SellStoryApplication.java
+++ src/main/java/kr/co/sellstory/SellStoryApplication.java
@@ -2,10 +2,16 @@ package kr.co.sellstory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SellStoryApplication {
 
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SellStoryApplication.class, args);
     }
